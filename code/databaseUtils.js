db4b47b10e326eb2e17a898204793eab34a25f7e
Jeff Hykin <jeff.hykin@gmail.com>
2019-04-20 20:24:43 -0500

    _

--- code/databaseUtils.js
+++ code/databaseUtils.js
@@ -25,15 +25,15 @@ let makeRequest = async (phrase, requestFunction) => {
 module.exports = {
     songsWithExactPreprocessedTitle: async (phrase) => {
         // return songTable.filter(each => each.preprocessedTitle == title)
-        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=song_title:" + phrase )
+        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=title:" + phrase )
     },
     songsWithPreprocessedTitleThatStartsWith: (phrase) => {
         // return songTable.filter(each => each.preprocessedTitle.startsWith(title))
-        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=song_title:" + phrase + "*" )
+        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=title:" + phrase + "*" )
     },
     songsWhosePreprocessedTitleContains: (phrase) => {
         // return songTable.filter( each => each.preprocessedTitle.match(phrase))
-        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=song_title:/.*" + phrase + ".*/")
+        return makeRequest(phrase, (phrase) =>"http://localhost:8983/solr/songs/select?q=title:/.*" + phrase + ".*/")
     },
     wordExistsInAtLeastOneSong: (word) => allWords[word],
     thereIsAWordThatStartsWith: (partialWord) => {
